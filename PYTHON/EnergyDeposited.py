840c7f4e712a1520c80f0d30af134c59adf41542
AlmaNucPhysics <77625265+AlmaNucPhysics@users.noreply.github.com>
2021-04-27 18:07:18 +0200

    Update EnergyDeposited.py

--- PYTHON/EnergyDeposited.py
+++ PYTHON/EnergyDeposited.py
@@ -1,24 +1,27 @@
-#Import the libraries
-
+@author: almakurmanova
+"""
+#%%
 import pandas as pd
 import matplotlib.pyplot as plt 
 import numpy as np
-
-infile=open('C:/Users/kurma/Desktop/Master thesis/DepositedEnergy.csv')
-
-E=pd.read_csv(infile, names=('Z', 'Energy'))
-E['Energy']=pd.to_numeric(E['Energy'], errors='coerce')                                           
-Energies=E['Energy']                                       
-z=E['Z']
-
-
+#%%
+infile=open('/Applications/topas/examples/Thesis/Energy1.csv')
+#%%
+data=pd.read_csv(infile, skiprows=8, names=('X','Y','Z', 'E'))
+data['E']=pd.to_numeric(data['E'])                                           
+Energy=data['E']                                       
+z=data['Z']
+#%%
 plt.figure()
 plt.grid()
-plt.plot(E['Z'],E['Energy'])
-plt.xlabel('z,bin of 4 cm')
-plt.ylabel('Energy, MeV')
-plt.title('Deposited energy along z axis')
+plt.plot(data['Z'],data['E'])
+plt.xlabel('z,bin of 0.0001 cm cm')
+plt.ylabel('E, MeV')
+plt.title('Energy deposited along z axis')
 plt.show()
 
+#%%
+sum_column = data.sum(axis=0)
+print (sum_column)
 
 
