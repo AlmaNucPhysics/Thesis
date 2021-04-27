495311a7ce75f623c630456ad245b34fddd022bb
AlmaNucPhysics <77625265+AlmaNucPhysics@users.noreply.github.com>
2021-04-27 18:11:19 +0200

    Update phsp_reader.py

--- PYTHON/phsp_reader.py
+++ PYTHON/phsp_reader.py
@@ -1,56 +1,137 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 13 16:24:15 2021
-
-@author: kurma
+@author: almakurmanova
 """
 
-#%% PhaseSpace
+#%% Libraries
+
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+from matplotlib import cm
 from topas2numpy import read_ntuple
+import pandas as pd
+import pylab as plb
+from lmfit import Parameters, minimize, report_fit, Model
+from mpl_toolkits.mplot3d import Axes3D
+from scipy.stats import multivariate_normal
+import seaborn as sns
+from scipy.optimize import curve_fit
 
 #%%
-x = read_ntuple('/Applications/topas/examples/Thesis/ASCIIOutput.header')
+data= read_ntuple('/Applications/topas/examples/Thesis/PhSp1.phsp')
 
-#%%
-X=np.zeros(x.shape)
-Y=np.zeros(x.shape)
-Z=np.zeros(x.shape)
-dircosX=np.zeros(x.shape)
-dircosY=np.zeros(x.shape)
-E=np.zeros(x.shape)
-Weight=np.zeros(x.shape)
-ID=np.zeros(x.shape)
+#%%Array creation
 
-#%%
-for i in range(len(x)):
-    X[i],Y[i],Z[i],dircosX[i],dircosY[i],E[i],Weight[i],ID[i],*rest=x[I]
+X=np.zeros(data.shape)
+Y=np.zeros(data.shape)
+Z=np.zeros(data.shape)
+dircosX=np.zeros(data.shape)
+dircosY=np.zeros(data.shape)
+E=np.zeros(data.shape)
+Weight=np.zeros(data.shape)
+Particle=np.zeros(data.shape)
+Flag_to_tell_if_third_direction_cosine_is_negative=np.zeros(data.shape)
+Flag_to_tell_if_this_is_the_first_scored_particle_from_this_history=np.zeros(data.shape)
 
-#%% 
-proton_energy=E[ID==2212]
-proton_x=X[ID==2212]
-proton_y=Y[ID==2212]
-proton_dircosX=dircosX[ID==2212]
-proton_dircosY=dircosY[ID==2212]
-#%%
-plt.hist(proton_energy)
-plt.xlabel('Energy, MeV')
+#%% Separation of the data 
+
+for i in range(len(data)):
+    X[i],Y[i],Z[i],dircosX[i],dircosY[i],E[i],Weight[i],Particle[i],Flag_to_tell_if_third_direction_cosine_is_negative[i],Flag_to_tell_if_this_is_the_first_scored_particle_from_this_history[i],*rest=data[i]
+
+#%% Selection of proton information
+
+proton_energy=E[Particle==2212]
+proton_x=X[Particle==2212]
+proton_y=Y[Particle==2212]
+proton_dircosX=dircosX[Particle==2212]
+proton_dircosY=dircosY[Particle==2212]
+
+#%%Energy distribution
+
+plt.hist(proton_energy,bins=100)
+plt.grid()
+plt.xlabel('Energy [MeV]')
 plt.ylabel('Histories')
 plt.title('Proton energy')
 plt.show()
 
-#%%
+#%%2D Histogram of particle distribution
+
+H,xedges,yedges=np.histogram2d(proton_x,proton_y,bins=10)
+X_val=(xedges[:-1]+xedges[1:])/2
+Y_val=(yedges[:-1]+yedges[1:])/2
+
+x,y=np.meshgrid(X_val,Y_val)
+
+fig = plt.figure(figsize =(10,10))
+ax = plt.axes(projection ='3d')
+surf=ax.plot_surface(x,y,H,cmap=cm.coolwarm,linewidth=0,antialiased=False) 
+plt.xlabel('X [cm]',fontweight='bold')
+plt.ylabel('Y [cm]',fontweight='bold')
+fig.colorbar(surf,ax=ax,shrink=0.7,aspect=7)
+plt.show()
+
+#%% LeastSQ Fitting 
+delX=X_val[1]-X_val[0]
+delY=Y_val[1]-Y_val[0]
+
+def gaussian2D(x, y, cen_x, cen_y, sig_x, sig_y):
+    return np.exp(-(((cen_x-x)/sig_x)**2 + ((cen_y-y)/sig_y)**2)/2.0) 
+
+def residuals(p, x, y, z):
+    height = p["height"].value
+    cen_x = p["centroid_x"].value
+    cen_y = p["centroid_y"].value
+    sigma_x = p["sigma_x"].value
+    sigma_y = p["sigma_y"].value
+    return (z - (height*delX*delY/(2*3.14*sigma_x*sigma_y))*gaussian2D(x,y, cen_x, cen_y, sigma_x, sigma_y))
+
+initial = Parameters()
+initial.add("height",value=1e6)
+initial.add("centroid_x",value=0.001)
+initial.add("centroid_y",value=0.001)
+initial.add("sigma_x",value=.15)
+initial.add("sigma_y",value=.15)
+
+fit = minimize(residuals, initial, args=(x, y, H))
+print(report_fit(fit))
+
+#%% Test of the model
+test=fit.params['height'].value*delX*delY/(2*3.14*fit.params['sigma_x'].value*fit.params['sigma_y'].value)*gaussian2D(x,y,fit.params['centroid_x'].value,fit.params['centroid_y'].value,fit.params['sigma_x'].value,fit.params['sigma_y'].value)
+                         
+fig = plt.figure(figsize =(10,10))
+ax = plt.axes(projection ='3d')
+surf=ax.plot_surface(x,y,test,cmap=cm.coolwarm,linewidth=0,antialiased=False) 
+plt.xlabel('X [cm]',fontweight='bold')
+plt.ylabel('Y [cm]',fontweight='bold')
+fig.colorbar(surf,ax=ax,shrink=0.7,aspect=7)
+plt.show()
+
+#%% Visualize the results (model and data)
+fig = plt.figure(figsize = (50,50))
+ax = Axes3D(fig)
+ax.plot_wireframe(x,y,test, label = 'Model')
+ax.scatter(x, y, H,color ='r', label = 'Data')
+plt.xlabel('X [cm]',fontweight='bold')
+plt.ylabel('Y [cm]',fontweight='bold')
+
+plt.legend()
+
+#%% Particle distribution
 
 plt.figure()
-plt.scatter(proton_x,proton_y)
+plt.scatter(proton_x,proton_y,label="data")
 plt.xlabel('X, cm')
 plt.ylabel('Y, cm')
 plt.title('Proton distibution')
 plt.grid()
 plt.show()
 
-#%%
+#%% Joint graph for partcile distribution
+
+df=pd.DataFrame({"x":proton_x,"y":proton_y})
+sns.jointplot(data=df,x='x',y='y',kind='resid')
+plt.grid()
+
+#%% PHASE SPACE IN X
 
 plt.figure()
 plt.scatter(proton_x,np.arccos(proton_dircosY))
@@ -59,7 +140,7 @@ plt.ylabel('X`, rad')
 plt.title('Beam Phase-Space distibution')
 plt.grid()
 plt.show()
-#%%
+#%% PHASE SPACE IN Y
 
 plt.figure()
 plt.scatter(proton_y,np.arccos(proton_dircosY))
